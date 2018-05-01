fd2d8512d0fdfcf9536f4c8a3e6b5461aa9dfa00
creeper <cjp736817298@126.com>
2018-05-01 13:44:03 +0800

    device list add modal

--- application/index/controller/Device.php
+++ application/index/controller/Device.php
@@ -7,6 +7,20 @@ use think\Session;
 class Device extends Controller
 {
 	public function index(){
+		$dev=Db::query("select * from link l,device d where l.vid=d.vid");
+		$this->assign('devices',$dev);
 		return $this->fetch();
+	}
+
+	public function add(){
+
+	}
+
+	public function delete($vid){
+		if(Db::name("link")->where('vid',$vid)->delete()){
+			$this->success("删除成功");
+		}else{
+			$this->error("删除失败");
+		}
 	}
 }
