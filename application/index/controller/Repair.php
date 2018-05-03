b5e658077cb869313e816b18e890fc1b3cfb532e
creeper <cjp736817298@126.com>
2018-05-03 16:00:16 +0800

    complete repair mudal

--- application/index/controller/Repair.php
+++ application/index/controller/Repair.php
@@ -13,7 +13,46 @@ class Repair extends Controller
 	}
 
 	public function add(){
-		// $pic = request()->file('pic');
-		// var_dump($pic);
+		$vid=input("param.vid");
+		$content=input("param.content");
+		//uid替换
+		$info=Db::name("link")->where('vid',$vid)->where('uid',1)->count();
+		if($info==0)
+			$this->error("当前用户与该设备未关联");
+		$data=[
+			//uid替换
+			"uid"=>1,
+			"vid"=>$vid,
+			"content"=>$content,
+			"uploadtime"=>date("Y-m-d H:i:s")
+		];
+		if(Db::name("repair")->insert($data))
+			$this->success("申请报修成功");
+		else
+			$this->error("申请报修失败");
+	}
+
+	public function addPic(){
+		$pic = request()->file('pic');
+		$tid=input("param.tid");
+		if($pic)
+		{
+			foreach ($pic as $key => $value) {
+				$info=$value->rule("uniqid")->validate(["size"=>3000000,"ext"=>"jpg,png,gif,jpeg"])->move(ROOT_PATH.'public'.DS.'static'.DS.'user'.DS.'image');
+				if($info)
+				{
+					$data=[
+						"tid"=>$tid,
+						"pic"=>$info->getFilename(),
+						"uploaddate"=>date("Y-m-d H:i:s")
+					];
+					if(!Db::name("repairphoto")->insert($data))
+						$this->error("第".($key+1)."张设备照片上传失败");
+				}
+				else
+					$this->$pic->getError();
+			}
+			$this->success("共".(count($pic))."张设备照片上传成功");
+		}
 	}
 }
