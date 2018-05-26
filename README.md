f4df26ae639caaf93c34e45ae458527be21b5003
bleach103 <huangjuntao15@nou.com.cn>
2018-05-26 20:59:01 +0800

    big fix for android API

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 ## The TGAM_SmartWheelChair_Managment_System User Console
  - PHP 7.0+
  - based on ThinkPHP 5.0
-## how to deployed on your server?
+## how to install on your server?
  - centos/ubuntu sever
  ```shell
     git clone https://github.com/bleach103/TGAM_SmartWheelChair_Managment_System502_106.git wheelchair
--- application/index/behavior/LoginCheck.php
+++ application/index/behavior/LoginCheck.php
@@ -14,8 +14,14 @@ class LoginCheck
 {
     public function run()
     {
+        //不需要检测是否登录的模块的控制器
+        $no_check = [
+            'index/user',
+        ];
+        //当前访问的模块,控制器
+        $visit = strtolower(Request::instance()->module().'/'.Request::instance()->controller());
         //已经登录返回true
-        if(Session::has("userinfo"))
+        if(Session::has("userinfo")||in_array($visit,$no_check))
         {
             return true;
         }
